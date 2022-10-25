b69f79680b45127559e478b49c4ad86682338f19
Steve Cassidy <scassidy@redhat.com>
2022-10-25 12:34:27 +0100

    variablise part of script

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+variable ami_to_use {
+  type = string
+  default = "ami-0648ea225c13e0729"
+}
+
+variable keypair_to_use {
+  type = string
+  default = "*** PUT SSH PUBLIC KEY HERE ***"
+}
+
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
@@ -65,7 +75,7 @@ resource "aws_route" "internet_route" {
 
 resource "aws_key_pair" "keypair1" {
   key_name   = "keypair1"
-  public_key = "*** PUBLIC KEY HERE ***"
+  public_key = var.keypair_to_use
 }
 
 resource "aws_security_group" "pub_sg" {
@@ -116,7 +126,7 @@ resource "aws_network_interface" "nic_1" {
 }
 #
 resource "aws_instance" "instance1" {
-  ami           = "ami-0648ea225c13e0729"
+  ami           = var.ami_to_use
   instance_type = "t2.micro"
   network_interface {
     network_interface_id = aws_network_interface.nic_1.id
